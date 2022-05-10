46366029a735a38982bb30f0a0855bb2c87ecd8f
Natanael Vieira <natanaelvieira130@gmail.com>
2022-05-10 16:50:26 -0300

    M1S11 - Exercicio2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Exercicio01Component } from './components/exercicio01/exercicio01.component';
+import { Exercicio02Component } from './components/exercicio02/exercicio02.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Exercicio01Component
+    Exercicio01Component,
+    Exercicio02Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/exercicio02/exercicio02.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio02',
+  templateUrl: './exercicio02.component.html',
+  styleUrls: ['./exercicio02.component.scss']
+})
+export class Exercicio02Component implements OnInit {
+  modeOn: boolean = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
